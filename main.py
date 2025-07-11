d6a1fea494dc13f22b665b7f9341914f2c3e4832
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-11 21:15:49 +0800

    fix: ctrl+c err handler
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ from runner import run
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s [%(levelname)s] (%(name)s): %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 if __name__ == '__main__':
-    Base.metadata.create_all(engine)
-    run()
+    try:
+        Base.metadata.create_all(engine)
+        run()
+    except KeyboardInterrupt:
+        print('\nOperation interrupted by user')
+    except Exception as e:
+        print(f'\nAn error occurred: {e}')
