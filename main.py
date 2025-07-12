23bca388a574d5090c5122cdb8c7584c4445b085
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-12 18:47:19 +0800

    feat: force exit
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import logging
+import multiprocessing
+import os
+import signal
+import sys
 
 from db import engine
 from db_model import Base
@@ -7,13 +11,37 @@ from runner import run
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s [%(levelname)s] (%(name)s): %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S')
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
-if __name__ == '__main__':
+interrupted = False
+worker = None
+
+
+def worker_main():
     try:
-        Base.metadata.create_all(engine)
         run()
     except KeyboardInterrupt:
-        print('\nOperation interrupted by user')
-    except Exception as e:
-        print(f'\nAn error occurred: {e}')
+        sys.exit(0)
+
+
+def handle_sigint(signum, frame):
+    global interrupted, worker
+    if not interrupted:
+        interrupted = True
+        if worker and worker.is_alive():
+            os.kill(worker.pid, signal.SIGINT)
+        print("\nReceived interrupt, shutting down...")
+    else:
+        os._exit(1)
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
+    signal.signal(signal.SIGINT, handle_sigint)
+
+    worker = multiprocessing.Process(target=worker_main)
+    worker.start()
+
+    while worker.is_alive():
+        worker.join(timeout=0.5)
